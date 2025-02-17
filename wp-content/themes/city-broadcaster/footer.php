8563dbd0f44cfcddcb7d110644b92bf342b305c0
Image Shandlani <ShandlaniIN@sabc.co.za>
2025-02-17 19:22:11 +0200

    custom theme footer done

--- wp-content/themes/city-broadcaster/footer.php
+++ wp-content/themes/city-broadcaster/footer.php
@@ -1,3 +1,119 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span class="custom-heading">Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4 custom-heading">
+                        <i class="fas fa-gem me-3 custom-heading"></i>City Casts
+                    </h6>
+                    <p>
+                        Here you can use rows and columns to organize your footer content. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 custom-heading">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Radio</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Wacth</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Podcasts</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 custom-heading">
+                        Other Links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Presenters</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Events</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Archive</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 custom-heading">Contact</h6>
+                    <p><i class="fas fa-home me-3"></i> Soweto, GP 1819, RSA</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        shondlanihlayiso@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); color: #777">
+        © 2025 Copyright:
+        <a class="text-reset fw-bold" href="#">City Casts</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </body>
 
 </html>
